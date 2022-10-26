047c24c657322158b01664e8a9376f5708edce37
ZoranDEV <zoranpavicevic098@gmail.com>
2022-10-26 21:29:33 +0200

    in myticket.js page change message when user do not have any tickets

--- src/components/myProfile/MyTicktes.js
+++ src/components/myProfile/MyTicktes.js
@@ -1,5 +1,7 @@
 // react
 import { useContext } from "react";
+// router dom
+import { Link } from "react-router-dom";
 // context
 import MainContext from "../../contexts/MainContext";
 // components
@@ -14,7 +16,19 @@ const MyTicktes = () => {
   return (
     <div>
       {tickets.length === 0 ? (
-        <div>No tickets yet</div>
+        <div>
+          <h1>No tickets yet.</h1>
+          <Link
+            to="/soccer"
+            children={
+              <button
+                className={`w-[280px] py-2 px-2 my-4 bg-blue-400 text-white cursor-pointer hover:bg-blue-500 transform-colors duration-[300ms] flex items-center justify-center rounded`}
+              >
+                See what's on offer
+              </button>
+            }
+          />
+        </div>
       ) : (
         <div>
           {tickets.map((ticket, index) => (
