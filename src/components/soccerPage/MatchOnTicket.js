758ce45db0c03c81ea471dc691ebadd5a6507641
ZoranDEV <zoranpavicevic098@gmail.com>
2022-10-27 17:09:46 +0200

    solved bug in matchonticket.js

--- src/components/soccerPage/MatchOnTicket.js
+++ src/components/soccerPage/MatchOnTicket.js
@@ -5,23 +5,11 @@ import SoccerContext from "../../contexts/SoccerContext";
 // icons
 import { FaTrash } from "react-icons/fa";
 
-const MatchOnTicket = ({ info: { id, game, oddValue } }) => {
+const MatchOnTicket = ({
+  info: { id, game, oddValue, commence_time, home_team, away_team },
+}) => {
   // context
   const { matchesToShow, deleteFromTicket } = useContext(SoccerContext);
-
-  // state for played match
-  const [playedMatch, setPlayedMatch] = useState(null);
-
-  useEffect(() => {
-    getInfo();
-  }, []);
-
-  // getInfo
-  const getInfo = () => {
-    matchesToShow.forEach((match) => {
-      match.id === id && setPlayedMatch(match);
-    });
-  };
 
   // handleOnClick
   const handleOnClick = () => {
@@ -32,7 +20,7 @@ const MatchOnTicket = ({ info: { id, game, oddValue } }) => {
     <div className="w-full mb-1 p-2 border-[1px] border-blue-500 text-white">
       {/* Title */}
       <div className="w-full mb-1 border-b-[1px] border-b-white flex items-center justify-between">
-        <h1>{playedMatch && playedMatch.commence_time.slice(0, 10)}</h1>
+        <h1>{commence_time.slice(0, 10)}</h1>
         <FaTrash
           onClick={handleOnClick}
           className="cursor-pointer hover:text-red-500 hover:transition-all hover:duration-[300ms] hover:scale-[1.2]"
@@ -41,8 +29,8 @@ const MatchOnTicket = ({ info: { id, game, oddValue } }) => {
       {/* Body */}
       <div className="w-full flex items-center justify-between">
         <div className="w-[60%] flex flex-col text-sm">
-          <h1>{playedMatch && playedMatch.home_team}</h1>
-          <h1>{playedMatch && playedMatch.away_team}</h1>
+          <h1>{home_team}</h1>
+          <h1>{away_team}</h1>
         </div>
         <div className="w-[70px] flex items-center justify-end text-sm">
           <h1 className="w-[40px] h-[30px] mr-1 bg-blue-600 text-white flex items-center justify-center">
